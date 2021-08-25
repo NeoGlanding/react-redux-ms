2583d1d6e3a39e99d31233a9e0bd5eaa0810ed41
NeoGlanding <neoglanding@protonmail.com>
2021-08-25 22:55:45 +0700

    redux subscription in functional component

--- src/components/CounterFunctional.jsx
+++ src/components/CounterFunctional.jsx
@@ -1,12 +1,15 @@
-import React, {useState} from 'react'
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux';
 
 const CounterFunctional = props => {
-    const [counter, setCounter] = useState(0);
+    const dispatch = useDispatch();
+    const counter = useSelector(state => state.counter);
 
     return (
         <div>
             <h1>Functional Counter - {counter}</h1>
-            <button onClick={() => setCounter(state => state + 1)}>Add Functional Counter</button>
+            <button onClick={() => dispatch({type: 'increment'})}>Add Functional Counter</button>
+            <button onClick={() => dispatch({type: 'decrement'})}>Decrement Functional Counter</button>
         </div>
     )
 }
