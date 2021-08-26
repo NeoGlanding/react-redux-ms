609d997f3ee15ba64ef758007493e0f71402d8ab
NeoGlanding <neoglanding@protonmail.com>
2021-08-26 10:26:19 +0700

    adding redux reducer toolkit

--- src/storage/reduxStore.js
+++ src/storage/reduxStore.js
@@ -1,19 +1,48 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    counter: 0
+    counter: 0,
+    toggler: true
 }
 
-const reducer = (state = initialState, action) => {
+createSlice({
+    name: 'counterToggler',
+    initialState,
+    reducers: {
+        increment(state) {
+            state.counter++
+        },
+        decrement(state) {
+            state.counter--
+        },
+        toggle(state) {
+            state.toggler = !state.toggler
+        }
+    }
+})
+
+const reducer = (state = initialState, action) => {    
     if (action.type === 'increment') {
         return {
-            counter: state.counter + 1
+            counter: state.counter + action.payload,
+            toggler: state.toggler
         }
     }
 
     if (action.type === 'decrement') {
         return {
-            counter: state.counter - 1
+            counter: state.counter + action.payload,
+            toggler: state.toggler
+
+        }
+    }
+
+    if (action.type === 'toggle') {
+        return {
+            counter: state.counter,
+            toggler: !state.toggler
+
         }
     }
 
